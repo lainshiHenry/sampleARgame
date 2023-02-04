332ed7affeb6d4524b6657880f12de659f84f669
Henry Le <henryle913@outlook.com>
2023-02-04 10:19:32 -0600

    first commit

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1654602.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 120386473, 120386458, 120386510, 120386487, 120386457, 120386489, 120386506, 120386467, 120386508, 120386472, 120386476, 120386512, 120386502, 120386484 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
